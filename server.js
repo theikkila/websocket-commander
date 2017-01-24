44efcdfe5e0658bc082d25ec2b56dc7c2ca96230
Valta, Robert J <robert.j.valta@gmail.com>
2017-01-24 19:58:22 +0200

    Add POST endpoint to parse application/json and x-www-form-urlencoded data

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
-const express = require('express')
-const app = express()
 const PORT = process.env.port || 3000
 
-// A mock to store groups to a static array
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const redis = require('redis')
+
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io').listen(server)
+
+const pub = redis.createClient()
+const sub = redis.createClient()
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Parse application/json
+app.use(bodyParser.json())
+
+// A mock to store groups to a static groups object
 const groups = {
   1: {online: 5},
   2: {online: 3}
 }
 
+// Returns true or false if a group belongs to groups
+const hasGroup = (group) => group in groups
+
+// Listen on the connection event for incoming sockets and join them a specified room.
+io.on('connection', (socket) => {
+  socket.on('OPEN', (msg) => {
+    socket.join(msg)
+  })
+})
+
 // GET /api/v0/:groupId/users
 // See how many group's users are online
 app.get('/api/v0/:groupId/users', (req, res) => {
   const groupId = parseInt(req.params.groupId, 10)
-  const hasGroup = (group) => group in groups
 
   if(!hasGroup(groupId)) {
     res.json({online: 0})
@@ -22,11 +47,21 @@ app.get('/api/v0/:groupId/users', (req, res) => {
 
 // POST /api/v0/:groupId/command
 // Send a COMMAND message to group's users
-//app.post('/api/v0/:groupId/command', (req,res) => {
-//
-//})
+app.post('/api/v0/:groupId/command', (req,res) => {
+  const groupId = req.params.groupId
+  const command = req.body.command
+
+  if(!hasGroup(groupId)) {
+    res.json({ack: false})
+  }
+
+  // Convey command message to a specified group
+  io.to(groupId).emit('COMMAND', command)
+
+  res.json({ack: true})
+})
 
 // Uses either environment variable port or default 3000 if none declared
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Express listening on port ' + PORT + '!')
 })
